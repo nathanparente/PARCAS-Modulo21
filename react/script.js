4a49f953622be2453b4ba1a9af18b93ec5e3690e
Nathan Parente <nathan.parente22@gmail.com>
2022-06-28 09:12:31 -0300

    Aula03

--- react/script.js
+++ react/script.js
@@ -1,6 +1,17 @@
+let nome = 'Antonio'
+let sobrenome = 'Lacerda'
 
+let titulo = <h2>{nome}</h2>
+
+let elemento = {
+    <div>
+        {titulo}
+        <p>{sobrenome}</p>
+        <small>Seja Bem Vindo</small>
+    </div>
+}
 
 ReactDOM.render(
-    'Olá Mundo',
+    elemento,
     document.getElementById("app")
 )
