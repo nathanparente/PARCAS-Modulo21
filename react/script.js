92e6f335135dca29910e4bdfe5c5e7c43a367038
Nathan Parente <nathan.parente22@gmail.com>
2022-06-28 10:53:05 -0300

    Aula09

--- react/script.js
+++ react/script.js
@@ -1,56 +1,31 @@
-/*
-function BemVindo(){
-    return (
-        <div>
-            <h1>Seja Bem Vindo</h1>
-            <h2>{props.nome} ({props.idade} anos)</h2>
-        </div>
-    )
-}
-*/
+class Relogio extends React.Component {
 
-class BemVindo extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            hora:'00:00:00'
+        };
+
+        setInterval(()=>{
+            this.setState({hora:new Date().toLocaleDateString()})
+        }, 1000)
+    }
+
+
     render(){
-        return 
+        return(
             <div>
-                <PostTopo autor={this.props.nome}/>
-                <PostCorpo/>
-                <PostRodape likes={this.props.likes} comentarios={this.props.comentarios} />
-
-            </div>       
-    }
-}
-
-class PostTopo extends React.Component {
-    render(){
-        return <div>{this.props.autor}</div>
-    }
-}
-
-class PostCorpo extends React.Component {
-    render(){
-        return <div>...</div>
-    }
-}
-
-class PostRodape extends React.Component {
-    render(){
-        return (
-            <div>
-                <p>{this.props.likes} curtidas</p>
-                <p>{this.props.comentarios} comentarios</p>
-                <hr/>
+                <h1>Horário</h1>
+                <h2>{this.state.hora}</h2> 
             </div>
         )
     }
 }
 
+
 let elemento = (
     <div>
-        <Post nome="Bonieky" likes="100" comentarios="15"/>
-        <Post nome="Paulo" likes="50" comentarios="340"/>
-        <Post nome="Jessica" likes="250" comentarios="200"/>
-        <Post nome="Outro Cara" likes="1000" comentarios="2"/>
+        <Relogio/>
     </div>
 )
 
