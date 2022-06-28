1f6e9ae7816a806f5c76cbd0cb9fd7b6b59ff3eb
Nathan Parente <nathan.parente22@gmail.com>
2022-06-28 10:03:21 -0300

    Aula08

--- react/script.js
+++ react/script.js
@@ -1,3 +1,4 @@
+/*
 function BemVindo(){
     return (
         <div>
@@ -6,12 +7,50 @@ function BemVindo(){
         </div>
     )
 }
+*/
+
+class BemVindo extends React.Component {
+    render(){
+        return 
+            <div>
+                <PostTopo autor={this.props.nome}/>
+                <PostCorpo/>
+                <PostRodape likes={this.props.likes} comentarios={this.props.comentarios} />
+
+            </div>       
+    }
+}
+
+class PostTopo extends React.Component {
+    render(){
+        return <div>{this.props.autor}</div>
+    }
+}
+
+class PostCorpo extends React.Component {
+    render(){
+        return <div>...</div>
+    }
+}
+
+class PostRodape extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>{this.props.likes} curtidas</p>
+                <p>{this.props.comentarios} comentarios</p>
+                <hr/>
+            </div>
+        )
+    }
+}
 
 let elemento = (
     <div>
-        <BemVindo nome="Nathan" idade="20"/>
-        <hr/>
-        <BemVindo nome="Bonieky" idade="99"/>
+        <Post nome="Bonieky" likes="100" comentarios="15"/>
+        <Post nome="Paulo" likes="50" comentarios="340"/>
+        <Post nome="Jessica" likes="250" comentarios="200"/>
+        <Post nome="Outro Cara" likes="1000" comentarios="2"/>
     </div>
 )
 
