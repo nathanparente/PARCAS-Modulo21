29afac8074d2bf16954982ca4331681fa328388c
Nathan Parente <nathan.parente22@gmail.com>
2022-06-29 13:20:32 -0300

    Aula20 Conversor de Moedas Via Dupla

--- react/script.js
+++ react/script.js
@@ -1,16 +1,14 @@
-class Temperatura extends React.Component {
-    render(){
-        let temp = parseFloat(this.props.temp);
-        if(this.props.escala == "f"){
-            temp = (temp * 9 / 5) + 32
-        }
 
+
+class Moeda extends React.Component {
+    render(){
+        let novoValor = parseFloat(this.props.valor);
 
         return(
             <div>
-                Temperatura em {this.props.escala.toUpperCase()} ----
-                {temp}
-                {this.props.escala == 'c' ? '°C' : '°F'}
+                Câmbio em: {this.props.moeda}<br/><br/>
+                {this.props.moeda == "dollar" ? novoValor/5 +' $' : novoValor*5 +' R$'}
+                
             </div>
         )
     }
@@ -21,22 +19,29 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            temperatura:20
+            valor:5
         }
 
-        this.tChange = this.tChange.bind(this)
+        this.vChange = this.vChange.bind(this)
+        this.inputChange = this.inputChange.bind(this)
     }
 
-    tChange(e){
-        this.setState({temperatura:e.target.value})
+    vChange(novoValor){
+        this.setState({valor:novoValor})
+    }
+
+    inputChange(e){
+        this.setState({valor:e.target.value})
     }
 
     render(){
         return(
             <div>
-                <input type="number" value={this.state.temperatura} onChange={this.tChange} />
-                <Temperatura temp={this.state.temperatura} escala="c"/>
-                <Temperatura temp={this.state.temperatura} escala="f"/>
+                <h3>Digite o valor para saber o câmbio:</h3>
+                <h4>Valor em Dólar vira Real ; Valor em Real vira Dólar</h4>
+                <input type="number" value={this.state.valor} onChange={this.inputChange}/><br/><br/>
+                <Moeda valor={this.state.valor} moeda="dollar" mudar={this.vChange}/><br/><br/>
+                <Moeda valor={this.state.valor} moeda="real" mudar={this.vChange}/><br/><br/>
             </div>
         )
     }
