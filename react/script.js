e0fb667d9105bf80d1405c9fe40c6ab5dce90399
Nathan Parente <nathan.parente22@gmail.com>
2022-06-29 09:05:28 -0300

    Aula18 Ações fora do Componente

--- react/script.js
+++ react/script.js
@@ -1,93 +1,46 @@
-class Login extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            formValues:{
-                email:'teste@gmail.com',
-                texto:'Algum texto ...',
-                sexo:'Feminino',
+class Botao extends React.Component {
 
-            }
-        }
-
-        this.inputTrocou = this.inputTrocou.bind(this)
-    }
-
-    inputTrocou(e){
-        let formValues = this.state.formValues;
-        formValues[e.target.name] = e.target.value;
-        this.setState({formValues})
-    }
 
     render(){
         return(
-            <form method='POST'>
-                <input type="email" name="email" value={this.state.formValues.email} onChange={this.inputTrocou} /><br/><br/>
-                <input type="password" name="senha" /><br/><br/>
-                <textarea name="corpo" value={this.state.formValues.texto} ></textarea>
-                <select name="opcoes" value={this.state.formValues.sexo} onChange ={this.sexoMudou}>
-                    <option></option>
-                    <option value="masculino">Masculino</option>
-                    <option value="feminino">Feminino</option>
-
-                </select>
-                <input type="submit" name="enviar" /><br/><br/>
-                Sexo: {this.state.formValues.sexo}
-            </form>
+            <button onClick={() => {
+                if(this.props.status === 1){
+                    this.props.funcaoTrocar(0)
+                }else {
+                    this.props.funcaoTrocar(1)
+                }
+            }}>{this.props.status === 1 ? 'DESABILITADO' : 'HABILITADO'}</button>
         )
     }
 }
 
-let elemento = (
-    <div>
-        <Login/>
-    </div>
-)
 
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            status:1
+        }
+
+        this.trocarStatus = this.trocarStatus.bind(this)
+    }
+
+    trocarStatus(novoStatus){
+        this.setState({status:novoStatus})
+    }
+
+
+    render(){
+        return(
+            <div>
+                <p>{this.state.status === 1 ? 'Habilitado' : 'Desabilitado'}</p>
+                <Botao status={this.state.status} funcaoTrocar={this.trocarStatus}/>
+            </div>
+        )
+    }
+}
 ReactDOM.render(
-    elemento, 
+    <App/>, 
     document.getElementById("app")
 )
-
-/* TIPOS DE CONDICIONAIS INLINE 
-
-*1
-
-constructor(props){
-        super(props)
-        this.state = {
-        msg: [1,2,3,4,5,6,7,8,9]
-        }
-    }
-
-    render(){
-        return(
-            <div>
-            {this.state.msg.length > 0 && 
-                <h2>Você possui {this.state.msg.length} mensagens não lidas</h2>
-            }
-                
-            </div>
-        )
-    }
-
-*2 
-
-class Aviso extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-        logado: false 
-        }
-    }
-
-    render(){
-        return(
-            <div>
-                O usuário {this.state.logado ? '' : 'não'} está logado
-            </div>
-        )
-    }
-}
-
-*/
