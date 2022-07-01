dd818216e3455223dc483b900827734adcf04234
Nathan Parente <nathan.parente22@gmail.com>
2022-07-01 08:03:41 -0300

    Aula22 Exercício To Do com busca

--- react/script.js
+++ react/script.js
@@ -1,48 +1,9 @@
-class Light extends React.Component {
-    render(){
-        const turnOn = () => {
-            document.getElementById('1').classList.remove("red")
-            document.getElementById('3').classList.add("green")
-        }
-
-        const turnOff = () => {
-            document.getElementById('3').classList.remove("green")
-            let yesYellow = document.getElementById('2').classList.add("yellow")
-            if(yesYellow == yesYellow){
-                setTimeout(() => {
-                    document.getElementById('2').classList.remove("yellow")
-                    document.getElementById('1').classList.add("red")
-                }, 1800)
-            }
-        }
-        return(
-            <div className="background">
-                <div className="semaforo">
-                    <div id='1' className="luz-semaforo"></div>
-                    <br/>
-                    <div id='2' className="luz-semaforo"></div>
-                    <br/>
-                    <div id='3' className="luz-semaforo"></div>
-                    <br/>
-                </div>
-                <div className="area-botao">
-                    <button onClick={turnOn}>Abrir Sinal</button>
-                    <button onClick={turnOff}>Fechar Sinal</button>
-                </div>
-            </div>
-        )
-    }
-}
-
-
-
-
 
 class App extends React.Component {
 
     render(){
         return(
-            <Light/>
+            <ToDoList/>
         )
     }
 }
